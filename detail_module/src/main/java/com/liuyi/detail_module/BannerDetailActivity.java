fc20bbeffda0441ea3bf15dcf3ec98591f25f44c
liuyi <17376865670@gcall.com>
2018-05-25 11:05:43 +0800

    添加ARouter实现组件间通信

--- detail_module/src/main/java/com/liuyi/detail_module/BannerDetailActivity.java
+++ detail_module/src/main/java/com/liuyi/detail_module/BannerDetailActivity.java
@@ -3,11 +3,14 @@ package com.liuyi.detail_module;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/detail/banner")
+public class BannerDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.com_activity_main);
+        setContentView(R.layout.md_activity_banner_detail);
     }
 }
